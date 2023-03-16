d5535597b4f599e6e5476f2b0f1ea201e24c22be
GaoXiong <xiong.gao@shopee.com>
2023-03-16 21:29:06 +0800

    chore: update

--- src/views/project/detail/components/base-info/index.vue
+++ src/views/project/detail/components/base-info/index.vue
@@ -7,8 +7,8 @@
       <div class="demand-item__bd">
         <div class="column">
           <span class="label">任务标签：</span>
-          <InlineTag :text="detailData.appType" />
-          <InlineTag :text="detailData.projectType + '任务'" />
+          <InlineTag :text="detailData.task_type_name" />
+          <InlineTag :text="detailData.category_name" />
         </div>
         <div class="column">
           <span class="label">需求预算：</span>
@@ -16,24 +16,24 @@
         </div>
         <div class="column">
           <span class="label">技能要求：</span>
-          <span v-html="detailData.requires || '无要求'"></span>
+          <span v-html="detailData.skill || '无要求'"></span>
         </div>
         <div class="column">
           <span class="label">发布日期：</span>
-          <span>{{ detailData.createTime }}</span>
+          <span>{{ detailData.create_time }}</span>
         </div>
         <div class="column">
           <span class="label">截止日期：</span>
-          <span v-if="detailData.deadline">
-            {{ detailData.deadline }}
+          <span v-if="detailData.create_time">
+            {{ detailData.create_time }}
           </span>
           <span v-else>无要求</span>
         </div>
       </div>
       <div class="demand-item__ft">
         <div class="ui-flex">
-          <img class="demand-item__avatar" :src="detailData.userInfo.avatar || avatar" />
-          <div class="demand-item__name">{{ detailData.userInfo.nickname }}</div>
+          <img class="demand-item__avatar" :src="detailData.author_avatar || avatar" />
+          <div class="demand-item__name">{{ detailData.author_name }}</div>
         </div>
         <div class="ui-flex enroll-info">
           <van-icon class="icon-people" name="friends" />
@@ -45,21 +45,21 @@
     <div class="floor floor-desc">
       <div class="floor__header">需求描述</div>
       <div class="floor__body">
-        <!-- <div v-html="decodeText()"></div> -->
+        {{ detailData.description }}
       </div>
     </div>
   </template>
 </template>
-  
+
 <script lang="ts" setup>
 import { defineProps } from 'vue'
-
 import InlineTag from '@/components/inline-tag/index.vue'
 import avatar from '@/assets/img/avatar.png'
-defineProps({
-  detailData: Object as any
-})
+import type { ProjectItemVo } from '@/service/auto-service/项目模块'
+
+defineProps<{
+  detailData: ProjectItemVo
+}>()
 </script>
-  
+
 <style lang="less" scoped src="./index.less"></style>
-  
--- src/views/project/detail/index.vue
+++ src/views/project/detail/index.vue
@@ -1,35 +1,38 @@
 <template>
-  <div class="view" v-if="detailData">
+  <Page title="项目详情" v-if="detailData">
     <BaseInfo :detail-data="detailData" />
     <EnrollPerson :enroll-list="enrolls" />
-  </div>
+  </Page>
 </template>
 
 <script lang="ts" setup>
-import { useRoute } from 'vue-router';
-import { ref, onMounted } from 'vue';
-import * as ProjectAPI from '@/service/project'
-import BaseInfo from './components/base-info/index.vue';
-import EnrollPerson from './components/enroll-person/index.vue';
+import { useRoute } from 'vue-router'
+import { ref, onMounted } from 'vue'
+import * as ProjectAPI from '@/service/auto-service/项目模块'
+import BaseInfo from './components/base-info/index.vue'
+import EnrollPerson from './components/enroll-person/index.vue'
+import Page from '@/components/page/index.vue'
 
-const route = useRoute();
-const enrolls = ref([] as any[]);
-const detailData = ref(null as any);
+const route = useRoute()
+const enrolls = ref([] as any[])
+const detailData = ref(null as any)
 
 const getDetails = async () => {
-  let res = await ProjectAPI.getProjectDetail()
-  detailData.value = res.data;
+  let res = await ProjectAPI.queryDetail({
+    id: Number(route.query.id)
+  })
+  detailData.value = res.data
 }
 
 const getEnrolls = async () => {
-  let res = await ProjectAPI.getOrderList()
-  enrolls.value = res.data.list;
+  // let res = await ProjectAPI.getOrderList()
+  // enrolls.value = res.data.list;
 }
 
 onMounted(() => {
-  getDetails();
-  getEnrolls();
-});
+  getDetails()
+  getEnrolls()
+})
 </script>
 
 <style lang="less" scoped src="./index.less"></style>
