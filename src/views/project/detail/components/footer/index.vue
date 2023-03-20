817a92d21f45910e9f0650bf69e5882e18734c25
GaoXiong <xiong.gao@shopee.com>
2023-03-20 10:10:42 +0800

    chore

--- src/views/project/detail/components/footer/index.vue
+++ src/views/project/detail/components/footer/index.vue
@@ -1,29 +1,49 @@
 <template>
-  <div class="group ui-flex group-btm">
-    <div class="edit-tip">
-      <van-icon name="edit" />
-      <span>说点什么</span>
+  <div class="group group-btm">
+    <div v-if="isEdit" class="edit-box ui-flex ui-flex-align-start">
+      <van-field
+        class="input"
+        v-model="message"
+        rows="1"
+        autosize
+        type="textarea"
+        placeholder="请输入留言"
+      />
+      <van-button class="ui-ml-10" size="small" type='default'>发送</van-button>
+      <van-button class="ui-ml-10" size="small" type="default" @click="hideEdit">取消</van-button>
     </div>
-    <div>
-      <van-space :size="18">
-      <div class="ui-flex">
-        <van-icon class="icon" name="like-o" />
-        <span>10</span>
+    <div class="action-box ui-flex" v-else>
+      <div class="edit-tip" @click="showEdit">
+        <van-icon name="edit" />
+        <span>说点什么</span>
       </div>
-      <div class="ui-flex">
-        <van-icon class="icon" name="star-o" />
-        <span>10</span>
+      <div>
+        <van-space :size="18">
+        <div class="ui-flex">
+          <van-icon class="icon" name="like-o" />
+          <span>10</span>
+        </div>
+        <div class="ui-flex">
+          <van-icon class="icon" name="star-o" />
+          <span>10</span>
+        </div>
+        <div class="ui-flex">
+          <van-icon class="icon" name="chat-o" />
+          <span>10</span>
+        </div>
+        </van-space>
       </div>
-      <div class="ui-flex">
-        <van-icon class="icon" name="chat-o" />
-        <span>10</span>
-      </div>
-      </van-space>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
+
+const isEdit = ref(false);
+const message = ref('');
+const showEdit = () => isEdit.value = true;
+const hideEdit = () => isEdit.value = false;
 </script>
 
 <style lang="less" scoped>
@@ -32,11 +52,29 @@
   margin-right: 4px;
 }
 
-.edit-tip {
-  height: 32px;
-  padding: 0 12px;
-  padding-right: 100px;
-  border-radius: 16px;
-  background-color: #f5f5f5;
+.edit-box {
+  flex: 1;
+  padding: 12px;
+  .input {
+    flex: 1;
+    padding: 6px 12px;
+    border-radius: 16px;
+    background-color: #f5f5f5;
+  }
+}
+
+.action-box {
+  flex: 1;
+  padding: 10px 12px;
+  .edit-tip {
+    display: flex;
+    align-items: center;
+    color: #888;
+    height: 32px;
+    padding: 0 12px;
+    padding-right: 100px;
+    border-radius: 16px;
+    background-color: #f5f5f5;
+  }
 }
 </style>
